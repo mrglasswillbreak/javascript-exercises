b7293796051543b9d28dc9b5950b4a9c0fd38df2
mrglasswillbreak <mrglasswillbreak@gmail.com>
2026-02-24 02:16:26 +0000

    completed excersises 6 to 11 which involved making use of loops and arrays

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+	if (num < 0) return "Error";
+	let string = "";
+	for (let i = 0; i < num; i++) {
+		string += string;
+	}
+	return string;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+//achieved result using math.min and math.max methods, 
 
+const sumAll = function(a, b) {
+  if (
+    (a < 0 || b < 0) ||
+    (!Number.isInteger(a) || !Number.isInteger(b))
+  ) {
+    return "ERROR";
+  }
+
+  const min = Math.min(a, b);
+  const max = Math.max(a, b);
+
+  let sum = 0;
+
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
